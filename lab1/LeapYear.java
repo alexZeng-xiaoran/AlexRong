4c4ca991fb36445a79fbcd160e7d2725982cd2b1
unknown <3068950154@qq.com>
2020-10-19 20:54:25 +0800

    submit my lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,6 +13,18 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+	/** discriminate a year is leap year or not
+	@param the year to be analyzed
+	*/
+	public static boolean isLeapYear(int year){
+		if(year%400==0){
+			return true;
+		}else if(year%4==0 && year%100 !=0){
+			return true;
+		}else{
+			return false;
+		}
+	}
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
